dba438a87dac76229a6989f0bb321651fbea141f
Bayden Ibrahim <baydenibrahim@gmail.com>
2022-05-05 15:56:24 -0400

    a9

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ import express from "express";
 import helloController from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
 import cors from "cors";
+import mongoose from "mongoose";
 import tuitsController from "./controllers/tuits-controller.js";
 
 const app = express();
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+ || 'mongodb://localhost:27017/fsd'
+mongoose.connect(CONNECTION_STRING);
+
 app.use(express.json());
 helloController(app);
 userController(app);
@@ -12,3 +17,5 @@ tuitsController(app);
 app.use(cors());
 
 app.listen(process.env.PORT || 4000);
+
+
